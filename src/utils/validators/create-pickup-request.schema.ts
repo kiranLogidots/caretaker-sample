6aeb3969a5f17d6c04252200f5c364a4e2095c5f
Ashraya KK <ashraya.kk@kensaltensi.org>
2024-02-07 22:52:11 +0530

    fix: Date error handling in form

--- src/utils/validators/create-pickup-request.schema.ts
+++ src/utils/validators/create-pickup-request.schema.ts
@@ -6,7 +6,10 @@ export const pickupReqFormSchema = z.object({
   collection_point_id: z.number().min(1, { message: messages.LSGIsRequired }),
   driver_id: z.number().min(1, { message: messages.driverIdIsRequired }),
   materialType: z.string().min(1, { message: messages.materialTypeRequired }),
-  date: z.string(),
-  approximateWeight: z.string(),
+  // date: z.string(),
+  date: z.string().refine((value) => !!value.trim(), {
+    message: messages.dateIsRequired,
+  }),
+  approximateWeight: z.string().min(1,{message:messages.weightRequired}),
 });
 export type PickupReqFormInput = z.infer<typeof pickupReqFormSchema>;
--- src/utils/validators/create-ward-data.schema.ts
+++ src/utils/validators/create-ward-data.schema.ts
@@ -4,7 +4,9 @@ import { messages } from '@/config/messages';
 // const validOrganisers = ['greenworms', 'lsg', 'other'];
 
 export const wardDataFormSchema = z.object({
-  date: z.string(),
+  date: z.string().refine((value) => !!value.trim(), {
+    message: messages.dateIsRequired,
+  }),
   collection_point_id: z
     .number()
     .min(1, { message: messages.collectionPointIdReq }),
@@ -17,17 +19,19 @@ export const wardDataFormSchema = z.object({
   house_paid: z.string().min(1, { message: messages.housePaid }),
   house_denied: z.string().min(1, { message: messages.houseDenied }),
   house_vacant: z.string().min(1, { message: messages.houseVacant }),
-  house_not_intrested: z
-    .string()
-    .min(1, { message: messages.houeNoInterest }),
+  house_not_intrested: z.string().min(1, { message: messages.houeNoInterest }),
   house_w_no_money: z.string().min(1, { message: messages.houseNoMoney }),
 
-  collection_amt: z.string().min(1, { message: messages.collectionAmountIsReq }),
+  collection_amt: z
+    .string()
+    .min(1, { message: messages.collectionAmountIsReq }),
   hks_incentive: z.string().min(1, { message: messages.hksIncentiveReq }),
 
   shops_not_cooperate: z.string().min(1, { message: messages.shopsDenied }),
   shops_with_no_money: z.string().min(1, { message: messages.shopsNoMoney }),
-  shops_with_no_interest: z.string().min(1, { message: messages.shopsNoInterest }),
+  shops_with_no_interest: z
+    .string()
+    .min(1, { message: messages.shopsNoInterest }),
   //   organised_by: z.string().min(1, { message: messages.organizedByIsRequired })
   //   .refine((value) => validOrganisers.includes(value), {
   //     message: messages.invalidOrganizer,
@@ -35,5 +39,5 @@ export const wardDataFormSchema = z.object({
   // ({
   //   required_error: messages.dateIsRequired,
   // }),
-})
+});
 export type WardDataFormInput = z.infer<typeof wardDataFormSchema>;
